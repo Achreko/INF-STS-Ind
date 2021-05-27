fa42acf9d5dfd9c7dc43fb0609837750ffaa5519
Achreko <achrekv3@gmail.com>
2021-05-28 00:17:31 +0200

    Blueprint of game screen is added.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+
+class GameScreen(GridLayout):
+       pass
 
 class Keybored(App):
     def build(self):
-        fl = FloatLayout()
-        for i in range(0,9):
-            fl.add_widget(Button(
-                background_color = (0,0,0,0)))
-
-        return fl
-
-if __name__ == "__main__":
-    Keybored().run()
+        return GameScreen()
+Keybored().run()
