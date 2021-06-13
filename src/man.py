5b5c1318f3e82fdb154a553e1636b5df6f04cfd6
Achreko <achrekv3@gmail.com>
2021-06-13 20:00:47 +0200

    Pop up for restarting after loss added

--- src/man.py
+++ src/man.py
@@ -1,4 +1,5 @@
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 class Man(ScreenManager):
@@ -7,7 +8,14 @@ class Man(ScreenManager):
 class MenuScreen(Screen):
     pass
 
+class Message(Popup):
+    def __init__(self, **kwargs):
+        super(Message, self).__init__(**kwargs)
+
 class GameScreen(Screen):
-    pass
+    def restart(self):
+        for child in self.children[0].children:
+            child.background_color = (1,1,1,1)
+            child.state = 'normal'
 
 root_widget = Builder.load_file('manager.kv')
